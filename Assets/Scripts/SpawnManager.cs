77a05d24c204855b3db6aecd00c8f28d4b2d7002
Drazen Drinic <drazendrinic@hotmail.com>
2024-10-15 18:58:33 +0200

    Code: animal spawn from left and right with boundaries implemented
    at the moment we can spawn them using Q for right and E for left spawning

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -12,6 +12,10 @@ public class SpawnManager : MonoBehaviour
     private float startDelay = 2.0f;
     private float spawnInterval = 1.5f;
 
+    public float sideSpawnMinZ;
+    public float sideSpawnMaxZ;
+    public float sideSpawnX;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,10 +25,19 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.C))
         {
-           SpawnRandomAnimal();
+            SpawnRandomAnimal();
         }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            SpawnRightAnimal();
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SpawnLeftAnimal();
+        }
+
     }
 
     void SpawnRandomAnimal()
@@ -32,5 +45,21 @@ public class SpawnManager : MonoBehaviour
         int animalIndex = Random.Range(0, animalPrefabs.Length);
         Vector3 spawnPosition = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
         Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
+    }
+
+    void SpawnLeftAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPosition = new Vector3(-sideSpawnX, 0, Random.Range(sideSpawnMinZ, sideSpawnMaxZ));
+        Vector3 rotationWhenSpawndOnTheLeft = new Vector3(0, 90, 0);
+        Instantiate(animalPrefabs[animalIndex], spawnPosition, Quaternion.Euler(rotationWhenSpawndOnTheLeft));
+    }
+
+    void SpawnRightAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPosition = new Vector3(sideSpawnX, 0, Random.Range(sideSpawnMinZ, sideSpawnMaxZ));
+        Vector3 rotationWhenSpawndOnTheRight = new Vector3(0, -90, 0);
+        Instantiate(animalPrefabs[animalIndex], spawnPosition, Quaternion.Euler(rotationWhenSpawndOnTheRight));
     }
 }

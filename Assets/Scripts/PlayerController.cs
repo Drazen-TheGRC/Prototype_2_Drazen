e9818423573fdad7f731e19a5ee73b57a2c5eee9
Drazen Drinic <drazendrinic@hotmail.com>
2024-10-15 18:01:11 +0200

    Add: player vertical movement with its boundaries

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float horizontalInput;
+    private float verticalInput;
+
     private float speed = 10.0f;
     private float xRange = 17.0f;
+
+    public float zMin;
+    public float zMax;
 
     public GameObject foodPrefabProjectile;
 
@@ -20,8 +25,9 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
+        // Player Horizontal movement
         horizontalInput = Input.GetAxis("Horizontal");
-        // Keep the player on the screen
+        // Keep the player on the screen horizontally
         if (transform.position.x < -xRange)
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
@@ -33,6 +39,21 @@ public class PlayerController : MonoBehaviour
        
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
 
+        // Player Vertical movement
+        verticalInput = Input.GetAxis("Vertical");
+        // Keep the player on the screen vertically 
+        if (transform.position.z < zMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMin);
+        }
+        if (transform.position.z > zMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zMax);
+        }
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+
+        // Spawn food projectile when space pressed 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // Launch a food projectile from the player
